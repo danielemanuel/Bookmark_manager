d7b27c4f335d48b889f089d41e850d9b2da1e871
danielemanuel <daniel.costea@vkernel.ro>
2017-05-01 12:30:32 +0100

    Initialize  the database

--- spec/features/testing_spec.rb
+++
@@ -1,7 +0,0 @@
-
-  feature 'Testing infrastructure' do
-    scenario 'Checks the apps if is running ' do
-      visit('/')
-      expect(page).to have_content     'Hey Daniel, I\'m ready to go!'
-    end
-  end
