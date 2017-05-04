a67ab0a88f645d514869c5fe1374268a59e29e8f
danielemanuel <daniel.costea@vkernel.ro>
2017-05-04 14:46:52 +0100

    Adding multiple tags to the link

--- app/models/link.rb
+++ app/models/link.rb
@@ -16,3 +16,4 @@ end
 # DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 # DataMapper.finalize
 # DataMapper.auto_upgrade!
+ 
--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -1,26 +1,27 @@
 feature 'Viewing links' do
-  before(:each) do
-      Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
-      Link.create(url: 'http://www.google.com', title: 'Google', tags: [Tag.first_or_create(name: 'search')])
-      Link.create(url: 'http://www.zombo.com', title: 'This is Zombocom', tags: [Tag.first_or_create(name: 'bubbles')])
-      Link.create(url: 'http://www.bubble-bobble.com', title: 'Bubble Bobble', tags: [Tag.first_or_create(name: 'bubbles')])
-    end
 
     scenario 'I can filter links by tag' do
+      Link.create(url: 'http://www.bubble-bobble.com', title: 'Bubble Bobble', tags: [Tag.first_or_create(name: 'bubbles')])
       visit '/tags/bubbles'
-
       expect(page.status_code).to eq(200)
       within 'ul#links' do
-        expect(page).not_to have_content('Makers Academy')
-        expect(page).not_to have_content('Code.org')
-        expect(page).to have_content('This is Zombocom')
-        expect(page).to have_content('Bubble Bobble')
+      expect(page).to have_content('Bubble Bobble')
       end
+    end
+
+    scenario 'user can add multiple tags to the url' do
+      visit '/links/new'
+      fill_in 'url',   with: 'http://www.makersacademy.com/'
+      fill_in 'title', with: 'Makers Academy'
+      fill_in 'tags',  with: 'education ruby'
+      click_button 'Create link'
+      link = Link.first
+      expect(link.tags.map(&:name)).to include('education', 'ruby')
     end
   end
 
 
-    #
+
     # scenario "The user can view a list of links associated with requested tag" do
     #   link = Link.create(url: 'http://www.google.com', title: 'Google')
     #   tag = Tag.create(tag_name: 'bubbles')
